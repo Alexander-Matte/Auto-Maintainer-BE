484b3777ddbf62d486f6e87fd2b7fd752b9b89d8
Alexander Matte <alex.matte71@gmail.com>
2024-08-05 20:13:12 +0200

    Fixed migrations

--- migrations/Version20240805181020.php
+++ migrations/Version20240805181020.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240805155720 extends AbstractMigration
+final class Version20240805181020 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -23,12 +23,13 @@ final class Version20240805155720 extends AbstractMigration
         $this->addSql('CREATE SEQUENCE car_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE SEQUENCE maintenance_activity_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE SEQUENCE "user_id_seq" INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE TABLE car (id INT NOT NULL, user_id_id INT NOT NULL, year INT DEFAULT NULL, make VARCHAR(255) DEFAULT NULL, model VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id))');
-        $this->addSql('CREATE INDEX IDX_773DE69D9D86650F ON car (user_id_id)');
-        $this->addSql('CREATE TABLE maintenance_activity (id INT NOT NULL, car_id INT DEFAULT NULL, name VARCHAR(255) NOT NULL, description VARCHAR(255) DEFAULT NULL, date TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE car (id INT NOT NULL, owner_id INT NOT NULL, make VARCHAR(255) DEFAULT NULL, year INT DEFAULT NULL, model VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_773DE69D7E3C61F9 ON car (owner_id)');
+        $this->addSql('CREATE TABLE maintenance_activity (id INT NOT NULL, car_id INT NOT NULL, name VARCHAR(255) NOT NULL, description VARCHAR(255) DEFAULT NULL, date DATE NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_B20C0D77C3C6F69F ON maintenance_activity (car_id)');
-        $this->addSql('CREATE TABLE "user" (id INT NOT NULL, email VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
-        $this->addSql('ALTER TABLE car ADD CONSTRAINT FK_773DE69D9D86650F FOREIGN KEY (user_id_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('CREATE TABLE "user" (id INT NOT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, created_at DATE DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('COMMENT ON COLUMN "user".created_at IS \'(DC2Type:date_immutable)\'');
+        $this->addSql('ALTER TABLE car ADD CONSTRAINT FK_773DE69D7E3C61F9 FOREIGN KEY (owner_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE maintenance_activity ADD CONSTRAINT FK_B20C0D77C3C6F69F FOREIGN KEY (car_id) REFERENCES car (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
     }
 
@@ -39,7 +40,7 @@ final class Version20240805155720 extends AbstractMigration
         $this->addSql('DROP SEQUENCE car_id_seq CASCADE');
         $this->addSql('DROP SEQUENCE maintenance_activity_id_seq CASCADE');
         $this->addSql('DROP SEQUENCE "user_id_seq" CASCADE');
-        $this->addSql('ALTER TABLE car DROP CONSTRAINT FK_773DE69D9D86650F');
+        $this->addSql('ALTER TABLE car DROP CONSTRAINT FK_773DE69D7E3C61F9');
         $this->addSql('ALTER TABLE maintenance_activity DROP CONSTRAINT FK_B20C0D77C3C6F69F');
         $this->addSql('DROP TABLE car');
         $this->addSql('DROP TABLE maintenance_activity');
